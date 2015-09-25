5969190eceb79503273acfca64ec43a2d19e91bf
unknown <4068927@HS222-18.vtsd.com>
2015-09-25 08:55:16 -0400

    Brendan Shine

---
+++ src/AWTGraphicsProgram.java
@@ -0,0 +1,49 @@
+// Lab04bvst.java
+// The AWT Graphics Program
+// This is the student, starting version of Lab04b.
+ /*************************************
+	 *                                   *
+	 * @author Brendan Shine             *
+	 * Mr. Kiedes Period 2               *
+	 * 9-25-15                           *
+	 *                                   *
+	 ************************************/
+	import java.awt.*;
+	import java.applet.*;
+
+
+	public class AWTGraphicsProgram extends Applet
+	{
+
+		public void paint(Graphics g)
+		{
+			// DRAW CUBE
+
+
+
+
+			// DRAW SPHERE
+
+
+
+
+			// DRAW INSCRIBED/CIRCUMSCRIBED TRIANGLE
+
+
+
+
+			// DRAW APCS
+
+
+
+
+			// DRAW PACMEN FLOWER
+
+
+
+
+		}
+
+	}
+
+
